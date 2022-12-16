4a0e6ce76d5d337fea189af5d629b7c256de9ee9
EC2 Default User <ec2-user@ip-172-31-8-197.eu-west-3.compute.internal>
2022-12-16 10:18:11 +0000

    ADJUSTED

--- dags/mysql_rds.py
+++ dags/mysql_rds.py
@@ -1,21 +1,21 @@
-import datetime
+# import datetime
 
-from airflow import DAG
-#from airflow.providers.postgres.operators.mysql import MySqlOperator
-from airflow.providers.mysql.operators.mysql import MySqlOperator
+# from airflow import DAG
+# #from airflow.providers.postgres.operators.mysql import MySqlOperator
+# from airflow.providers.mysql.operators.mysql import MySqlOperator
 
-# create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
-# instantiating the Postgres Operator
+# # create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
+# # instantiating the Postgres Operator
 
-with DAG(
-    dag_id="mysql_operator_dag",
-    start_date=datetime.datetime(2020, 2, 2),
-    schedule_interval="@once",
-    catchup=False,
-    tags=['testing-dags']
-) as dag:
-    create_pet_table = MySqlOperator(
-    task_id="create_pet_table",
-    mysql_conn_id="mysql_default",
-    sql="sql/mysql_query.sql",
-)
+# with DAG(
+#     dag_id="mysql_operator_dag",
+#     start_date=datetime.datetime(2020, 2, 2),
+#     schedule_interval="@once",
+#     catchup=False,
+#     tags=['testing-dags']
+# ) as dag:
+#     create_pet_table = MySqlOperator(
+#     task_id="create_pet_table",
+#     mysql_conn_id="mysql_default",
+#     sql="sql/mysql_query.sql",
+# )
